173e4eadfd13a5285209b394df03d94c3b5f31ed
gabireugabiru <gabigrenteski@gmail.com>
2022-06-27 13:54:06 -0300

    versions page

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ use actix_web::{get, web, App, HttpServer, Responder, HttpResponse};
 async fn index() -> impl Responder {
     HttpResponse::Ok().body(include_str!("templates/index.html"))
 }
-
 #[get("/guide")]
 async fn guide() -> impl Responder {
    HttpResponse::Ok().body(include_str!("templates/guide.html")) 
@@ -13,16 +12,29 @@ async fn guide() -> impl Responder {
 async fn docs() -> impl Responder {
     HttpResponse::Ok().body(include_str!("templates/docs.html"))
 }
+#[get("/versions")]
+async fn versions() -> impl Responder {
+    HttpResponse::Ok().body(include_str!("templates/versions.html"))
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let port = std::env::var("PORT").unwrap();
+    let port = match std::env::var("PORT") {
+        Ok(a) => a,
+        Err(_) => "8080".to_owned()
+    };
     
     HttpServer::new(|| {
         App::new()
-        .service(actix_files::Files::new("/static", "/app/static") )
+        .service(actix_files::Files::new("/static", match std::env::var("PORT") {
+            Ok(_) => "/app/static",
+            Err(_) => "./static"
+        }) )
             .route("/hello", web::get().to(|| async { "Hello World!" }))
             .service(index)
             .service(guide)
+            .service(docs)
+            .service(versions)
     })
     .bind(format!("0.0.0.0:{}", port))?
     .run()
---
+++ static/versions.js
@@ -0,0 +1,18 @@
+const versions = [
+    {
+        version: "0.0.5",
+        windows: "/static/versions/0.0.5vWindows.zip",
+        linux: "/static/versions/0.0.5vLinux.zip"
+    }
+];
+const list = document.getElementById("list");
+if (list) {
+    for (const version of versions) {
+        const child = document.createElement("li");
+        child.innerHTML = 
+        `<h3>${version.version}</h3>
+        <a href="${version.windows}">Windows</a>
+        <a href="${version.linux}">Linux</a>`;
+        list.appendChild(child);
+    }
+}
